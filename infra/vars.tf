bbabb1d924c9121f3f15015071d44874b1141135
Jake Smtih <work.jacobsmith@gmail.com>
2024-10-10 18:53:12 -0600

    added IaC infra

---
+++ infra/vars.tf
@@ -0,0 +1,23 @@
+variable "AWS_REGION" {
+  default = ""
+}
+
+variable "PATH_TO_PRIVATE_KEY" {
+  default = ""
+}
+
+variable "PATH_TO_PUBLIC_KEY" {
+  default = ""
+}
+
+variable "AMIS" {
+  default = ""
+}
+
+variable "db-password" {
+  default = ""
+}
+
+variable "AS_AMI" {
+  default = ""
+}
